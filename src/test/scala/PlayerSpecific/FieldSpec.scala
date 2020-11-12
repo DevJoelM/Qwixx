fcccf38f1d5bafa646d3aea7924e643fe02fcc9a
Joel Merath <joel.merath.dev@gmail.com>
2020-11-12 11:43:11 +0100

    Added TextUI with command possibility.

--- src/test/scala/PlayerSpecific/FieldSpec.scala
+++ src/test/scala/PlayerSpecific/FieldSpec.scala
@@ -1,16 +1,16 @@
-package src.test.scala
+package PlayerSpecific
 
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 /////////////////////////////////////////////////////////////
-// FileName: PlaygroundSpec.scala
+// FileName: FieldSpec.scala
 // FileType: Scala-Test Source file
 // Author: Joel Merath, Tim Disch
-// Created On : 05.11.2020
-// Last Modified On : 05.11.2020
+// Created On : 12.11.2020
+// Last Modified On : 12.11.2020
 /////////////////////////////////////////////////////////////
 
-class PlaygroundSpec extends AnyWordSpec with Matchers {
+class FieldSpec extends AnyWordSpec with Matchers{
 
 }
